eec43a20b2906dd29be5d58e238fe4f62153da40
Michal Dyczkowski <themich4@gmail.com>
2024-05-05 15:19:21 +0200

    Moved tailwind config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,15 +16,4 @@ return {
       require('nvim-tree').setup {}
     end,
   },
-  {
-    'luckasRanarison/tailwind-tools.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    opts = {}, -- your configuration
-  },
-  {
-    'laytan/tailwind-sorter.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-    build = 'cd formatter && npm ci && npm run build',
-    config = true,
-  },
 }
---
+++ lua/custom/plugins/tailwind.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'luckasRanarison/tailwind-tools.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {}, -- your configuration
+  },
+  {
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm ci && npm run build',
+    config = true,
+  },
+}
