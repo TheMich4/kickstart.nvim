1959a9bc2172f53ac90d80a25c39d45c43126e70
Michal Dyczkowski <themich4@gmail.com>
2024-10-06 13:00:53 +0200

    Added chad tab keybinds

--- lua/custom/plugins/chad.lua
+++ lua/custom/plugins/chad.lua
@@ -10,6 +10,31 @@ vim.keymap.set('n', '<leader>ns', function()
   require('minty.shades').open()
 end, { desc = '[N]v Color [S]hades' })
 
+-- Tabs
+vim.keymap.set('n', '<S-Tab>', function()
+  require('nvchad.tabufline').prev()
+end, { desc = 'Previous Tab' })
+
+vim.keymap.set('n', '<Tab>', function()
+  require('nvchad.tabufline').next()
+end, { desc = 'Next Tab' })
+
+vim.keymap.set('n', '<leader>bc', function()
+  require('nvchad.tabufline').close_buffer()
+end, { desc = '[B]uffer [C]lose' })
+
+vim.keymap.set('n', '<leader>bq', function()
+  require('nvchad.tabufline').closeAllBufs(true)
+end, { desc = '[B]uffer [Q]uit All' })
+
+vim.keymap.set('n', '<leader>br', function()
+  require('nvchad.tabufline').move_buf(1)
+end, { desc = '[B]uffer Move [R]ight' })
+
+vim.keymap.set('n', '<leader>bl', function()
+  require('nvchad.tabufline').move_buf(-1)
+end, { desc = '[B]uffer Move [L]eft' })
+
 return {
   {
     'nvchad/ui',
