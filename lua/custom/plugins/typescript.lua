8fba69a01c13d2202415fd074331140ceb574492
Michal Dyczkowski <themich4@gmail.com>
2024-11-30 12:03:29 +0100

    Added some plugins

--- lua/custom/plugins/typescript.lua
+++ lua/custom/plugins/typescript.lua
@@ -49,4 +49,21 @@ return {
       require('ts-error-translator').setup()
     end,
   },
+
+  {
+    'Wansmer/treesj',
+    keys = { '<space>m', '<space>j', '<space>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+    config = function()
+      require('treesj').setup {--[[ your config ]]
+      }
+
+      -- For default preset
+      vim.keymap.set('n', '<leader>m', require('treesj').toggle)
+      -- For extending default preset with `recursive = true`
+      vim.keymap.set('n', '<leader>M', function()
+        require('treesj').toggle { split = { recursive = true } }
+      end)
+    end,
+  },
 }
---
+++ lua/custom/plugins/window.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {}
+    end,
+  },
+
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = {},
+  },
+}
